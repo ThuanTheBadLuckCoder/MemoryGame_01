9562263866343fa19d265d6d852a7b59d8a3cac7
Tran Thuan <dtthuan.1211@gmail.com>
2023-12-29 23:52:46 +0700

    update giao dien moi

--- app/src/main/java/com/example/mymemorygame01/MainActivity.kt
+++ app/src/main/java/com/example/mymemorygame01/MainActivity.kt
@@ -1,118 +1,11 @@
 package com.example.mymemorygame01
 
-import android.graphics.Color
 import android.os.Bundle
-import android.os.Handler
-import android.view.Gravity
-import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.core.view.children
-import androidx.gridlayout.widget.GridLayout
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var gridLayout: GridLayout
-    private val gridSize = 6 // 6x6 grid
-    private val buttons = Array(gridSize) { arrayOfNulls<Button>(gridSize) }
-    private val correctButtons = mutableListOf<Button>()
-    private val handler = Handler()
-    private var playerScore = 60
-    private var penalty = 10
-    private var buttonsPressed = 0
-    private val maxButtonsToPress = 5
-    private var gameEnded = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        gridLayout = findViewById(R.id.gridLayout)
-
-        initializeGrid()
-    }
-
-    private fun initializeGrid() {
-        gridLayout.post {
-            val gridWidth = gridLayout.width - (gridLayout.paddingLeft + gridLayout.paddingRight)
-            val buttonSize = (gridWidth / gridSize) - 2 // Adjust for border
-
-            for (i in 0 until gridSize) {
-                for (j in 0 until gridSize) {
-                    buttons[i][j] = Button(this).also { button ->
-                        button.layoutParams = GridLayout.LayoutParams().apply {
-                            width = buttonSize
-                            height = buttonSize
-                            setMargins(1, 1, 1, 1)
-                        }
-                        button.background = ContextCompat.getDrawable(this, R.drawable.button_border)
-                        gridLayout.addView(button)
-                        button.setOnClickListener {
-                            checkButton(button)
-                        }
-                    }
-                }
-            }
-            displayRandomColors() // Ensure this is called to set up the game
-        }
-    }
-
-
-    private fun displayRandomColors() {
-        val random = Random(System.currentTimeMillis())
-        val numberOfColors = 5 // Number of colored blocks to show
-
-        correctButtons.clear()
-        for (button in gridLayout.children) {
-            (button as Button).setBackgroundColor(Color.WHITE)
-        }
-
-        repeat(numberOfColors) {
-            val i = random.nextInt(gridSize)
-            val j = random.nextInt(gridSize)
-            val button = buttons[i][j]!!
-            button.setBackgroundColor(Color.YELLOW)
-            correctButtons.add(button)
-        }
-
-        handler.postDelayed({
-            correctButtons.forEach { it.setBackgroundColor(Color.WHITE) }
-        }, 3000)
-    }
-
-    private fun checkButton(button: Button) {
-        if(gameEnded) {
-            // Nếu trò chơi đã kết thúc, không xử lý các nhấn nút nữa
-            return
-        }
-        if (button in correctButtons) {
-            button.setBackgroundColor(Color.GREEN)
-        } else {
-            button.setBackgroundColor(Color.RED)
-            playerScore -= penalty
-            penalty += 10 // Tăng hình phạt cho lần sai tiếp theo
-        }
-
-        buttonsPressed++
-        if (buttonsPressed >= maxButtonsToPress || playerScore <= 0) {
-            endGame()
-        }
-    }
-
-    private fun endGame() {
-        gameEnded = true
-        runOnUiThread {
-            Toast.makeText(this, "Game Over! Your score: $playerScore", Toast.LENGTH_LONG).show()
-            disableButtons()
-        }
-    }
-    private fun disableButtons() {
-        for (i in 0 until gridSize) {
-            for (j in 0 until gridSize) {
-                buttons[i][j]?.isEnabled = false
-            }
-        }
     }
 }
