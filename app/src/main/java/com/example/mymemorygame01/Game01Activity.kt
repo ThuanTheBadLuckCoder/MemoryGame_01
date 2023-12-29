fbc19e59a9f1c58f1925da9f630f24981de44221
Tran Thuan <dtthuan.1211@gmail.com>
2023-12-30 01:10:27 +0700

    update 31/12/2023

---
+++ app/src/main/java/com/example/mymemorygame01/Game01Activity.kt
@@ -0,0 +1,97 @@
+import android.graphics.Color
+import android.os.Bundle
+import android.os.Handler
+import android.view.Gravity
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
+import androidx.gridlayout.widget.GridLayout
+import com.example.mymemorygame01.R
+import kotlin.random.Random
+
+class Game01Activity : AppCompatActivity() {
+    private lateinit var gridLayout: GridLayout
+    private val gridSize = 6 // 6x6 grid
+    private val buttons = Array(gridSize) { arrayOfNulls<Button>(gridSize) }
+    private val correctButtons = mutableListOf<Button>()
+    private val handler = Handler()
+    private var playerScore = 60
+    private var penalty = 10
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.game01_activity)
+        gridLayout = findViewById(R.id.gridLayout)
+
+        initializeGrid()
+    }
+
+    private fun initializeGrid() {
+        gridLayout.post {
+            val gridWidth = gridLayout.width - (gridLayout.paddingLeft + gridLayout.paddingRight)
+            val gridHeight = gridLayout.height - (gridLayout.paddingTop + gridLayout.paddingBottom)
+
+            val buttonSize = Math.min(gridWidth, gridHeight) / gridSize
+
+            for (i in 0 until gridSize) {
+                for (j in 0 until gridSize) {
+                    buttons[i][j] = Button(this).apply {
+                        layoutParams = GridLayout.LayoutParams().apply {
+                            width = buttonSize - 10 * 2
+                            height = buttonSize - 10 * 2
+                            setMargins(5, 5, 5, 5)
+                            // Chỉnh layout_gravity để căn giữa theo chiều ngang và chiều dọc
+                            gravity = Gravity.CENTER
+                        }
+                        setBackgroundColor(Color.WHITE)
+                        setOnClickListener {
+                            checkButton(this)
+                        }
+                    }
+                    gridLayout.addView(buttons[i][j])
+                }
+            }
+
+            displayRandomColors()
+        }
+    }
+
+    private fun displayRandomColors() {
+        val random = Random(System.currentTimeMillis())
+        val numberOfColors = 5 // Number of colored blocks to show
+        correctButtons.clear()
+        for (button in gridLayout.children) {
+            (button as Button).setBackgroundColor(Color.WHITE)
+        }
+        repeat(numberOfColors) {
+            val i = random.nextInt(gridSize)
+            val j = random.nextInt(gridSize)
+            val button = buttons[i][j]!!
+            button.setBackgroundColor(Color.YELLOW)
+            correctButtons.add(button)
+        }
+        handler.postDelayed({
+            correctButtons.forEach { it.setBackgroundColor(Color.WHITE) }
+        }, 3000)
+    }
+
+    private fun checkButton(button: Button) {
+        if (button in correctButtons) {
+            button.setBackgroundColor(Color.GREEN)
+        } else {
+            button.setBackgroundColor(Color.RED)
+            playerScore -= penalty
+            penalty += 10 // Tăng hình phạt cho lần sai tiếp theo
+
+            if (playerScore <= 0) {
+                endGame()
+            }
+        }
+    }
+
+    private fun endGame() {
+        Toast.makeText(this, "Game Over! Your score: $playerScore", Toast.LENGTH_LONG).show()
+    }
+}
+
--- app/src/main/java/com/example/mymemorygame01/MainActivity.kt
+++ app/src/main/java/com/example/mymemorygame01/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.mymemorygame01
 
+import Game01Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonChoice1 = findViewById<Button>(R.id.buttonChoice1)
+        buttonChoice1.setOnClickListener {
+            val intent = Intent(this, Game01Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
+
