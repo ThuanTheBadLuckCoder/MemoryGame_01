7e2985fe5fa75386b19da65eb6ba0e4820ae6803
Tran Thuan <dtthuan.1211@gmail.com>
2023-12-30 18:27:39 +0700

    a little update for game01

--- app/src/main/java/com/example/mymemorygame01/Game01Activity.kt
+++ app/src/main/java/com/example/mymemorygame01/Game01Activity.kt
@@ -1,10 +1,12 @@
 package com.example.mymemorygame01
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
-import android.view.Gravity
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
@@ -24,8 +26,17 @@ class Game01Activity : AppCompatActivity() {
     private var correctPresses = 0
     private val totalColorsToShow = 5
     private var isDisplayingColors = false
+    private var score = 0
+
+    private lateinit var scoreTextView: TextView
+    private lateinit var highScoreTextView: TextView
+    private lateinit var sharedPreferences: SharedPreferences
+    private var highScore = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        sharedPreferences = getSharedPreferences("MyMemoryGamePrefs", Context.MODE_PRIVATE)
+        highScore = sharedPreferences.getInt("highScore", 0)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.game01_activity)
         gridLayout = findViewById(R.id.gridLayout)
@@ -33,6 +44,19 @@ class Game01Activity : AppCompatActivity() {
         initializeGrid()
         setupLayoutChangeListener()
 
+        val exitButton = findViewById<Button>(R.id.exitButton)
+        val restartButton = findViewById<Button>(R.id.restartButton)
+        scoreTextView = findViewById(R.id.scoreTextView)
+        highScoreTextView = findViewById(R.id.highScoreTextView)
+
+        highScoreTextView.text = "High Score: $highScore"
+
+        exitButton.setOnClickListener {
+            exitGame() // Gọi hàm exitGame() khi nút "Exit Game" được bấm
+        }
+        restartButton.setOnClickListener {
+            restartGame() // Gọi hàm restartGame() khi nút "Restart Game" được bấm
+        }
 
     }
 
@@ -123,31 +147,32 @@ class Game01Activity : AppCompatActivity() {
         }, 3000)
     }
 
-
-
     private fun checkButton(button: Button) {
         if (!isGameActive) {
             return
         }
-        if (button in correctButtons) {
+        if (button in correctButtons && !buttonStates[button]!!) { // Kiểm tra nếu nút chưa được bấm đúng trước đó
             button.setBackgroundColor(Color.GREEN)
             buttonStates[button] = true
             correctPresses++
+            score += 1
 
             if (correctPresses == totalColorsToShow) {
                 Toast.makeText(this, "Great! Starting next round...", Toast.LENGTH_SHORT).show()
                 resetGame()
             }
         } else {
-            button.setBackgroundColor(Color.RED)
-            mistakesMade++
+            if (buttonStates[button] == null || !buttonStates[button]!!) { // Kiểm tra nếu nút chưa được bấm đúng trước đó
+                button.setBackgroundColor(Color.RED)
+                mistakesMade++
 
-            if (mistakesMade > allowedMistakes) {
-                endGameWithLoss()
+                if (mistakesMade > allowedMistakes) {
+                    endGameWithLoss()
+                }
             }
         }
+        updateScore()
     }
-
 
     private fun endGameWithLoss() {
         isGameActive = false
@@ -160,7 +185,6 @@ class Game01Activity : AppCompatActivity() {
             resetGame()
         }
     }
-
 
     private fun resetGame() {
         if (mistakesMade <= allowedMistakes) {
@@ -184,5 +208,27 @@ class Game01Activity : AppCompatActivity() {
         finish()
     }
 
+    private fun restartGame() {
+        finish()
+        startActivity(intent)
+    }
 
+    private fun updateScore() {
+        // Hiển thị điểm lên TextView
+        scoreTextView.text = "Score: $score"
+
+        // Khi cập nhật điểm số
+        if (score > highScore) {
+            // Cập nhật điểm số cao nhất
+            highScore = score
+
+            // Lưu điểm số cao nhất vào SharedPreferences
+            val editor = sharedPreferences.edit()
+            editor.putInt("highScore", highScore)
+            editor.apply()
+
+            // Cập nhật hiển thị điểm số cao nhất
+            highScoreTextView.text = "High Score: $highScore"
+        }
+    }
 }
