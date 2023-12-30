d0813decb9207e359eb7e983eb65c51292480930
Tran Thuan <dtthuan.1211@gmail.com>
2023-12-30 23:02:10 +0700

    update for game 02

--- app/src/main/java/com/example/mymemorygame01/Game02Activity.kt
+++ app/src/main/java/com/example/mymemorygame01/Game02Activity.kt
@@ -1,25 +1,42 @@
 package com.example.mymemorygame01
 
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class Game02Activity : AppCompatActivity() {
+    private lateinit var cardImageViews: Array<ImageView>
+    private val cardImages = mutableListOf(
+        "card01", "card02", "card03", "card04", "card05",
+        "card06", "card07", "card08"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.game02_activity)
 
-        // Gọi hàm randomChoose() để chọn ngẫu nhiên hình ảnh cho ImageView thứ hai
-        randomChoose()
+        cardImageViews = arrayOf(
+            findViewById(R.id.cardImageView1),
+            findViewById(R.id.cardImageView2),
+            findViewById(R.id.cardImageView3)
+        )
+
+        showRandomCards()
     }
 
-    private fun randomChoose() {
-        val cardImageView = findViewById<ImageView>(R.id.cardImageView)
+    private fun showRandomCards() {
+        // Trộn danh sách lá bài
+        cardImages.shuffle()
 
-        val cardImages = arrayOf("card_1", "card_2", "card_3", "card_4", "card_5", "card_6", "card_7", "card_8", "card_9")
-        val randomImageName = cardImages.random()
-        val resourceId = resources.getIdentifier(randomImageName, "drawable", packageName)
-        cardImageView.setImageResource(resourceId)
+        // Hiển thị 3 lá bài đầu tiên từ danh sách đã được trộn
+        for (i in cardImageViews.indices) {
+            val cardImageView = cardImageViews[i]
+            cardImageView.tag = cardImages[i]
+            cardImageView.visibility = View.VISIBLE
+            val resourceId = resources.getIdentifier(cardImages[i], "drawable", packageName)
+            cardImageView.setImageResource(resourceId)
+        }
     }
 }
-
